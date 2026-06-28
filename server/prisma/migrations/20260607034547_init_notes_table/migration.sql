cde093e2d571088be3b4bdcea1877252a2ae3f25
Usman Ghani <usman.dev24@gmail.com>
2026-06-28 09:50:21 +0500

    server(chore/prisma): old migration removed, pirsma migrate reset

--- server/prisma/migrations/20260607034547_init_notes_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Notes" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "body" TEXT NOT NULL,
-    "createdAt" TIMESTAMPTZ(6) NOT NULL,
-
-    CONSTRAINT "Notes_pkey" PRIMARY KEY ("id")
-);
