3fa4eeb67dbc27f215107eb9c581ba06dec616a4
Usman Ghani <usman.dev24@gmail.com>
2026-06-29 14:27:14 +0500

    test (server/ auth) : implement tests for creating, login, and deleting users.

---
+++ server/tests/auth.test.mjs
@@ -0,0 +1,63 @@
+import { describe, it, expect, afterAll, beforeAll } from "vitest";
+import request from "supertest";
+
+import { app } from "../src/app.mjs";
+import UserStore from "../src/model/user-store.mjs";
+const userStore = new UserStore();
+
+afterAll(async () => {
+  await userStore.deleteAll("deleteAll");
+});
+
+describe("Testing Auth Endpoint", () => {
+  // Maintained the agent instance to track cookies across requests
+  const agent = request.agent(app);
+
+  it("should successfully create register new user", async () => {
+    const res = await agent
+      .post("/api/v1/auth/register")
+      .send({
+        userName: "test-user",
+        password: "test",
+        firstName: "test",
+        lastName: "user",
+        email: "test@example.com", 
+      })
+      .set("Content-Type", "application/json")
+      .accept("application/json");
+
+    expect(res.status).toBe(200);
+    expect(res.body.success).toBe(true);
+  });
+
+  it("should login test user", async () => {
+    const res = await agent
+      .post("/api/v1/auth/login")
+      .send({
+        userName: "test-user",
+        password: "test",
+      })
+      .set("Content-Type", "application/json")
+      .accept("application/json")
+      .redirects(1)
+    
+    expect(res.body.success).toBe(true);
+  });
+
+  it("should get test user", async () => {
+    const res = await agent
+      .get("/api/v1/users/me")
+      .accept("application/json");
+
+    expect(res.status).toBe(200);
+    expect(res.body.success).toBe(true);
+  });
+  it("should delete test user", async () => {
+    const res = await agent
+      .delete("/api/v1/users/me")
+      .accept("application/json");
+
+    expect(res.status).toBe(200); 
+    expect(res.body.success).toBe(true); 
+  });
+});
